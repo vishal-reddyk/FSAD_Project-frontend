ea45591890df46eaa44de8b98e5f3ddbb5ec6b35
vishalreddyk <2400080133@kluniversity.in>
2026-04-28 19:39:40 +0530

    Update frontend Firebase project config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,10 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAVqQIA2WkQe2FByVRbxHGuSDlUqnT8vnA",
-  authDomain: "fullstack-91ea3.firebaseapp.com",
-  projectId: "fullstack-91ea3",
-  appId: "1:216864558018:web:0957ebe32d696e742ba45b",
+  apiKey: "AIzaSyCNJRc87EdnzTVnf0DiaxYcSIxLpAwgWr4",
+  authDomain: "fullstack-91ea3-871c8.firebaseapp.com",
+  projectId: "fullstack-91ea3-871c8",
+  storageBucket: "fullstack-91ea3-871c8.firebasestorage.app",
+  messagingSenderId: "806145650",
+  appId: "1:806145650:web:bff47a18604ae7dea17ceb",
+  measurementId: "G-0N0RLXX7Y8",
 };
 
 const app = initializeApp(firebaseConfig);
